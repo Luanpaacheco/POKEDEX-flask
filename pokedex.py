3f24a2e7a26e5dddecc799f2f98640533f2fbe3a
Luan Pacheco Lima <luanlimapacheco@gmail.com>
2024-08-30 14:54:19 -0300

    consigo pesquisar o nome do pokemon e resultar na imagem dele, usando a pokeapi

--- pokedex.py
+++ pokedex.py
@@ -1,4 +1,8 @@
+from flask.globals import request
 from flask import Flask, render_template
+from models.pokemon import Pokemon
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -6,6 +10,22 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/buscar", methods=["GET", "post"])
+def buscar():
+    pokemon=Pokemon(request.form["nome"].lower(),"")
+    try:
+        res=json.loads(requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon.nome}").text)
+        result = res["sprites"]
+        result=result["front_default"]
+        pokemon.foto=result
+    except:
+        return "pokemon não encontrado"
+    return render_template('index.html', 
+    nome=pokemon.nome,
+    foto=pokemon.foto                       
+                           )
+    
 
 
 if __name__ == "__main__":
